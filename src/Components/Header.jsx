ee782a86b2f9b75ffe684dfad5c03765851438aa
DilettaZecchinetti <diletta.zecchinetti@gmail.com>
2025-01-04 13:12:08 +0000

    header and title size update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useUsers } from '../Contexts/UsersContext'
+import { useUsers } from '../Contexts/UsersContext';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -19,27 +19,32 @@ function Header() {
 
     return (
         <div className='header'>
-            <Navbar bg="dark" data-bs-theme="dark" fixed="top">
+            <Navbar bg="dark" data-bs-theme="dark" fixed="top" expand="lg">
                 <Container className='header'>
                     <Navbar.Brand href="/" className="nc-news-brand">NC NEWS</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/topics/coding">Coding</Nav.Link>
-                        <Nav.Link as={Link} to="/topics/football">Football</Nav.Link>
-                        <Nav.Link as={Link} to="/topics/cooking">Cooking</Nav.Link>
-                        <Nav.Link as={Link} to="/users">Users</Nav.Link>
-                    </Nav>
-                    <Navbar.Text className="ms-auto text-white">
-                        Logged in as: <strong>tickle122</strong>
-                    </Navbar.Text>
-                    {currentUser && currentUser.avatar_url && (
-                        <img
-                            src={currentUser.avatar_url}
-                            alt="User Avatar"
-                            className="user-avatar ms-3"
-                            style={{ width: '30px', height: '30px', borderRadius: '50%' }}
-                        />
-                    )}
+                    <Navbar.Toggle aria-controls="navbar-nav" />
+                    <Navbar.Collapse id="navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/topics/coding">Coding</Nav.Link>
+                            <Nav.Link as={Link} to="/topics/football">Football</Nav.Link>
+                            <Nav.Link as={Link} to="/topics/cooking">Cooking</Nav.Link>
+                            <Nav.Link as={Link} to="/users">Users</Nav.Link>
+                        </Nav>
+                        <div className="d-flex align-items-center ms-auto">
+                            <Navbar.Text className="text-white">
+                                Logged in as: <strong>tickle122</strong>
+                            </Navbar.Text>
+                            {currentUser && currentUser.avatar_url && (
+                                <img
+                                    src={currentUser.avatar_url}
+                                    alt="User Avatar"
+                                    className="user-avatar ms-3"
+                                    style={{ width: '30px', height: '30px', borderRadius: '50%' }}
+                                />
+                            )}
+                        </div>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
